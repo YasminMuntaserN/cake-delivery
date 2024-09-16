7b0d4d2b6656781a4ac6693bd7c79d7db4e76bb1
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-16 12:51:31 +0300

    feat: add Delivery DTO

---
+++ Backend/CakeDeliveryDTO/DeliveryDTO.cs
@@ -0,0 +1,27 @@
+﻿namespace DTOs
+{
+    public class DeliveryDTO
+    {
+        public DeliveryDTO(int deliveryId, int orderId, string deliveryAddress, string deliveryCity, string deliveryPostalCode, string deliveryCountry, DateTime deliveryDate, string deliveryStatus)
+        {
+            this.DeliveryID = deliveryId;
+            this.OrderID = orderId;
+            this.DeliveryAddress = deliveryAddress;
+            this.DeliveryCity = deliveryCity;
+            this.DeliveryPostalCode = deliveryPostalCode;
+            this.DeliveryCountry = deliveryCountry;
+            this.DeliveryDate = deliveryDate;
+            this.DeliveryStatus = deliveryStatus;
+        }
+
+        public int DeliveryID { get; set; }
+        public int OrderID { get; set; }
+        public string DeliveryAddress { get; set; }
+        public string DeliveryCity { get; set; }
+        public string DeliveryPostalCode { get; set; }
+        public string DeliveryCountry { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public string DeliveryStatus { get; set; }
+    }
+
+}
