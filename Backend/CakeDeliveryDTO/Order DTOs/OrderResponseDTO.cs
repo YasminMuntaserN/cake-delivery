3802ee4cc531375466fd6a2c0370c4c388b20535
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-18 18:18:33 +0300

    feat: add OrderResponseDTO

---
+++ Backend/CakeDeliveryDTO/Order DTOs/OrderResponseDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs
+{
+    /// <summary>
+    /// DTO Used for retrieving order details. 
+    /// </summary>
+    /// 
+
+    public record OrderResponseDTO(
+        int OrderID,
+        int CustomerID,
+        DateTime OrderDate,
+        decimal TotalAmount,
+        string PaymentStatus,
+        string DeliveryStatus
+    );
+
+}
