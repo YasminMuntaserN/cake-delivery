3af9bd10ecbdd40cb108fadec518c811750f4e40
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-18 18:16:06 +0300

    feat : add OrderCreateRequestDTO

---
+++ Backend/CakeDeliveryDTO/Order DTOs/OrderCreateRequestDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs
+{
+    /// <summary>
+    /// DTO Used for creating a new order
+    /// </summary>
+    /// 
+    public record OrderCreateRequestDTO(
+     int CustomerID,
+     decimal TotalAmount,
+     string PaymentStatus,
+     string DeliveryStatus
+ );
+
+}
