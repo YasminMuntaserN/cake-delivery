cd1b03ae549b6b5699f8fcdf1dbf154db153a797
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-18 18:32:47 +0300

    feat: add DeliveryUpdateFindRequestDTO

---
+++ Backend/CakeDeliveryDTO/DeliveryDTO/DeliveryCreateRequestDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace CakeDeliveryDTO.DeliveryDTO
+{
+    /// <summary>
+    /// DTO Used to update an existing delivery or retrieve details of a delivery.
+    /// </summary>
+    /// 
+    public record DeliveryUpdateFindRequestDTO(
+        int DeliveryID,
+        int OrderID,
+        string DeliveryAddress,
+        string DeliveryCity,
+        string DeliveryPostalCode,
+        string DeliveryCountry,
+        DateTime DeliveryDate,
+        string DeliveryStatus
+    );
+
+}
