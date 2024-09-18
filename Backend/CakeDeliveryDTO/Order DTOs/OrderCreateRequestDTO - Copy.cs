b0b50b203733a5246b0fc8557168630d63a3e2e3
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-18 18:17:30 +0300

    feat: add OrderUpdateRequestDTO

---
+++ Backend/CakeDeliveryDTO/Order DTOs/OrderCreateRequestDTO - Copy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs
+{
+    /// <summary>
+    /// DTO Used for updating an existing order
+    /// </summary>
+    /// 
+    public record OrderUpdateRequestDTO(
+        int OrderID,
+        int CustomerID,
+        decimal TotalAmount,
+        string PaymentStatus,
+        string DeliveryStatus
+    );
+
+}
