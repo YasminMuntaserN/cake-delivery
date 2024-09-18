41f6a1c6d4be07d53e62ba11bc5a021df72906e8
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-18 19:00:11 +0300

    feat : add  DataAccessHelper class

---
+++ Backend/DataAccessLayer/ErrorLogger.cs
@@ -0,0 +1,17 @@
+﻿namespace DataAccessLayer
+{
+    public class ErrorLogger
+    {
+        private readonly Action<string, Exception> _logAction;
+
+        public ErrorLogger(Action<string, Exception> logAction)
+        {
+            _logAction = logAction;
+        }
+
+        public void LogError(string message, Exception ex)
+        {
+            _logAction(message, ex);
+        }
+    }
+}
