1fb8b5869d8655884ea5d47e22d92ef6ae1b9993
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-21 07:45:35 +0300

    feat: add cake api controller

---
+++ Backend/CakeDeliveryAPI/Controllers/CakeApiController.cs
@@ -0,0 +1,138 @@
+﻿using Business_Layer;
+using CakeDeliveryDTO.CakeDTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CakeApiController : ControllerBase
+{
+    [HttpGet("All", Name = "GetAllCakes")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<IEnumerable<CakeDTO>> GetAllCakes()
+    {
+        List<CakeDTO> cakesList = clsCake.All();
+        if (cakesList.Count == 0)
+        {
+            return NotFound("No Cakes Found!");
+        }
+        return Ok(cakesList);
+    }
+
+   
+    [HttpGet("{id}", Name = "GetCakeById")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<CakeDTO> GetCakeById(int id)
+    {
+        if (id < 1)
+        {
+            return BadRequest($"Not accepted ID {id}");
+        }
+
+        CakeDTO? cake = clsCake.FindCakeById(id);
+        if (cake == null)
+        {
+            return NotFound($"Cake with ID {id} not found.");
+        }
+
+        return Ok(cake);
+    }
+
+ 
+    
+    [HttpGet("name/{cakeName}", Name = "GetCakeByName")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<CakeDTO> GetCakeByName(string cakeName)
+    {
+        CakeDTO? cake = clsCake.FindCakeByName(cakeName);
+        if (cake == null)
+        {
+            return NotFound($"Cake with name '{cakeName}' not found.");
+        }
+        return Ok(cake);
+    }
+
+   
+    
+    //[HttpPost(Name = "AddCake")]
+    //[ProducesResponseType(StatusCodes.Status201Created)]
+    //[ProducesResponseType(StatusCodes.Status400BadRequest)]
+    //public ActionResult<CakeDTO> AddCake([FromBody] CakeDTO newCakeDTO)
+    //{
+    //    if (newCakeDTO == null || string.IsNullOrEmpty(newCakeDTO.CakeName))
+    //    {
+    //        return BadRequest("Invalid cake data.");
+    //    }
+
+    //    clsCake cakeInstance = new clsCake(newCakeDTO, clsCake.enMode.AddNew);
+    //    if (cakeInstance.Save())
+    //    {
+    //        newCakeDTO?.CakeID = cakeInstance.CakeID;
+    //        return CreatedAtRoute("GetCakeById", new { id = newCakeDTO.CakeID }, newCakeDTO);
+    //    }
+
+    //    return BadRequest("Unable to create cake.");
+    //}
+
+    //[HttpPut("{id}", Name = "UpdateCake")]
+    //[ProducesResponseType(StatusCodes.Status200OK)]
+    //[ProducesResponseType(StatusCodes.Status400BadRequest)]
+    //[ProducesResponseType(StatusCodes.Status404NotFound)]
+    //public ActionResult<CakeDTO> UpdateCake(int id, [FromBody] CakeDTO updatedCake)
+    //{
+    //    if (id < 1 || updatedCake == null || string.IsNullOrEmpty(updatedCake.CakeName))
+    //    {
+    //        return BadRequest("Invalid cake data.");
+    //    }
+
+    //    CakeDTO? existingCake = clsCake.FindCakeById(id);
+    //    if (existingCake == null)
+    //    {
+    //        return NotFound($"Cake with ID {id} not found.");
+    //    }
+
+    //    clsCake cakeInstance = new clsCake(updatedCake, clsCake.enMode.Update);
+    //    if (cakeInstance.Save())
+    //    {
+    //        return Ok(cakeInstance.ToCakeDto());
+    //    }
+
+    //    return StatusCode(500, new { message = "Error updating cake." });
+    //}
+
+    //[HttpDelete("{id}", Name = "DeleteCake")]
+    //[ProducesResponseType(StatusCodes.Status200OK)]
+    //[ProducesResponseType(StatusCodes.Status404NotFound)]
+    //public ActionResult DeleteCake(int id)
+    //{
+    //    if (id < 1)
+    //    {
+    //        return BadRequest($"Not accepted ID {id}");
+    //    }
+
+    //    if (clsCake.Delete(id))
+    //    {
+    //        return Ok($"Cake with ID {id} has been deleted.");
+    //    }
+
+    //    return NotFound($"Cake with ID {id} not found. No rows deleted!");
+    //}
+
+    //[HttpGet("category/{category}", Name = "GetCakesByCategory")]
+    //[ProducesResponseType(StatusCodes.Status200OK)]
+    //[ProducesResponseType(StatusCodes.Status404NotFound)]
+    //public ActionResult<IEnumerable<CakeDTO>> GetCakesByCategory(string category)
+    //{
+    //    List<CakeDTO> cakesList = clsCake.All(category);
+    //    if (cakesList.Count == 0)
+    //    {
+    //        return NotFound($"No Cakes found in category '{category}'!");
+    //    }
+    //    return Ok(cakesList);
+    //}
+}
