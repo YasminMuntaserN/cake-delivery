931aa98129439093cb7997df61cdafd13b6071a0
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-21 06:31:48 +0300

    rename to cake dto to be general to use

---
+++ Backend/CakeDeliveryDTO/CakeDTOs/CakeDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CakeDeliveryDTO.CakeDTOs
+{
+    /// <summary>
+    /// DTO for Updating or Find a cake.
+    /// </summary>
+    /// 
+    public record CakeDTO(
+        int? CakeID,
+        string CakeName,
+        string Description,
+        decimal Price,
+        int StockQuantity,
+        string Category,
+        string ImageUrl
+    );
+
+}
