e92914ae76334d81badba7a8a92e3d7231305969
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-26 13:47:01 +0300

    feat: add clsValidationResult class

---
+++ Backend/Validation/clsValidationResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation
+{
+    public class clsValidationResult
+    {
+        public bool IsValid { get; private set; } = true;
+        public List<string> Errors { get; } = new List<string>();
+
+        public void AddError(string error)
+        {
+            IsValid = false;
+            Errors.Add(error);
+        }
+    }
+}
