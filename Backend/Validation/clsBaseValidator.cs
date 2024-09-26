278c92806ca6726eec8bd89abf67dc445c75ac23
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-26 13:55:42 +0300

    feat: add clsBaseValidator class

---
+++ Backend/Validation/clsBaseValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation
+{
+    public abstract class clsBaseValidator<T>
+    {
+        protected clsValidationResult Result { get; private set; } = new clsValidationResult();
+
+        public abstract ValidationResult Validate(T entity);
+
+        protected void AddError(string error)
+        {
+            Result.AddError(error);
+        }
+    }
+
+}
