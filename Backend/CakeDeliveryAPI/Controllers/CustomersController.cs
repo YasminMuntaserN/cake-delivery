c1d4e77b45a7c658ad4ee4e3b50fbc5afee58f49
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-28 15:51:31 +0300

    feat: add customers controller

---
+++ Backend/CakeDeliveryAPI/Controllers/CustomersController.cs
@@ -0,0 +1,177 @@
+﻿using Business_Layer.Customer;
+using CakeDeliveryDTO.CustomerDTOs;
+using DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Diagnostics.Metrics;
+using System.Net;
+
+namespace CakeDeliveryAPI.Controllers
+{
+
+    [Route("api/customers")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly clsCustomerValidator _validator = new clsCustomerValidator();
+
+
+        // GET: api/customers/all
+        [HttpGet("all", Name = "GetAllcustomers")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<IEnumerable<CustomerDTO>> GetAllcustomers()
+        {
+            List<CustomerDTO> customersList = clsCustomer.All();
+            if (customersList.Count == 0)
+            {
+                return NotFound("No customers Found!");
+            }
+            return Ok(customersList);
+        }
+
+
+        // GET: api/customers/{id}
+        [HttpGet("{id}", Name = "GetcustomerById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<CustomerDTO> GetcustomerById(int id)
+        {
+            if (id < 1)
+            {
+                return BadRequest($"Not accepted ID {id}");
+            }
+
+            CustomerDTO? customer = clsCustomer.FindCustomerById(id);
+            if (customer == null)
+            {
+                return NotFound($"customer with ID {id} not found.");
+            }
+
+            return Ok(customer);
+        }
+
+
+        // GET: api/customers/{Name}
+        [HttpGet("{Name}", Name = "GetcustomerByName")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<CustomerDTO> GetcustomerByName(string Name)
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                return BadRequest($"Not accepted Name {Name}");
+            }
+
+            CustomerDTO? customer = clsCustomer.FindCustomerByName(Name);
+            if (customer == null)
+            {
+                return NotFound($"customer with Name {Name} not found.");
+            }
+
+            return Ok(customer);
+        }
+
+
+        // POST: api/customers
+        [HttpPost(Name = "Addcustomer")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<CustomerDTO> Addcustomer([FromBody] CustomerCreateDTO newCustomerDTO)
+        {
+            if (newCustomerDTO == null)
+            {
+                return BadRequest("Invalid customer data.");
+            }
+            clsCustomer customerInstance = new clsCustomer(
+                new CustomerDTO(null, newCustomerDTO.FirstName, newCustomerDTO.LastName,string.Concat(newCustomerDTO.FirstName, " ", newCustomerDTO.LastName)
+                , newCustomerDTO.Email, newCustomerDTO.PhoneNumber, newCustomerDTO.Address, newCustomerDTO.City, newCustomerDTO.PostalCode, newCustomerDTO.Country, DateTime.Now),
+            clsCustomer.enMode.AddNew
+            );
+
+            // Validate the customer instance
+            var validationResult = _validator.Validate(customerInstance);
+            if (!validationResult.IsValid)
+            {
+                return BadRequest(new
+                {
+                    Success = false,
+                    Errors = validationResult.Errors
+                });
+            }
+
+            if (customerInstance.Save())
+            {
+                return CreatedAtRoute("GetcustomerById", new { id = customerInstance.CustomerID }, newCustomerDTO);
+            }
+            return BadRequest("Unable to create customer.");
+        }
+
+
+        // PUT: api/customers/{id}
+        [HttpPut("{id}", Name = "Updatecustomer")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<CustomerDTO> Updatecustomer(int id, CustomerDTO updatedcustomer)
+        {
+            if (id < 1 || updatedcustomer == null )
+            {
+                return BadRequest("Invalid customer data.");
+            }
+
+            CustomerDTO? existingcustomer = clsCustomer.FindCustomerById(id);
+            if (existingcustomer == null)
+            {
+                return NotFound($"customer with ID {id} not found.");
+            }
+
+            clsCustomer customerInstance = new clsCustomer(updatedcustomer, clsCustomer.enMode.Update);
+
+            // Validate the customer instance
+            var validationResult = _validator.Validate(customerInstance);
+            if (!validationResult.IsValid)
+            {
+                return BadRequest(new
+                {
+                    Success = false,
+                    Errors = validationResult.Errors
+                });
+            }
+
+            if (customerInstance.Save())
+            {
+                return Ok(customerInstance.TocustomerDto());
+            }
+
+            return StatusCode(500, new { message = "Error updating customer." });
+        }
+
+
+        // DELETE: api/customers/{id}
+        [HttpDelete("{id}", Name = "Deletecustomer")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult Deletecustomer(int id)
+        {
+            if (id < 1)
+            {
+                return BadRequest($"Not accepted ID {id}");
+            }
+
+            if (clsCustomer.Delete(id))
+            {
+                return Ok($"customer with ID {id} has been deleted.");
+            }
+
+            return NotFound($"customer with ID {id} not found. No rows deleted!");
+        }
+
+
+
+    }
+}
