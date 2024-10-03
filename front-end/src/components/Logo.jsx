784cfccd18c8ab99045c6a5bc4487bee5296dd4a
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-10-03 17:21:38 +0300

    feat: add Logo component

--- front-end/src/components/Logo.jsx
+++ front-end/src/components/Logo.jsx
@@ -3,7 +3,11 @@ import { Link } from "react-router-dom";
 function Logo() {
   return (
     <Link to="/">
-      <img src="/Logo.png" alt="CakeDelivery logo" />
+      <div className="logo text-center hover:cursor-pointer">
+        <img src="./logo.png" alt="CakeDelivery logo"className="w-[200px] object-contain  "/>
+      </div>
     </Link>
   );
 }
+
+export default Logo;
