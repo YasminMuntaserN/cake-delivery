2d35937da29697f0ba980ed30313e4cd5b9ecaff
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-10-05 15:22:42 +0300

    feat: add CategoryElement component

---
+++ front-end/src/components/Categories/CategoryElement.jsx
@@ -0,0 +1,20 @@
+function CategoryElement({Category}) {
+    return (
+      <div
+        
+        className="w-[350px] p-7 flex items-center gap-3 bg-[#fde4e4]  rounded cursor-pointer transition duration-400 hover:shadow-lg hover:scale-110
+        "
+      >
+        <div className="bg-pink w-[100px] h-[100px] rounded-full relative mr-10" >
+          <img
+            className="w-12 h-12 absolute top-[20%] left-[25%] "
+            src={Category.CategoryImageURL}
+            alt="category__item"
+          />
+        </div>
+        <h6>{Category.CategoryName}</h6>
+      </div>
+    );
+}
+
+export default CategoryElement
