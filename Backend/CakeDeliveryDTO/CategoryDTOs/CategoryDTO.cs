fb386dbb95920cad7f6f2066e5f3cb640993f96b
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-10-05 17:47:23 +0300

    feat : add category  dtos

---
+++ Backend/CakeDeliveryDTO/CategoryDTOs/CategoryDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CakeDeliveryDTO;
+
+/// <summary>
+/// DTO for Create a new Category.
+/// </summary>
+/// 
+public record CategoryDTO(
+    int? CategoryID,
+    string CategoryName,
+    string CategoryImageUrl
+);
