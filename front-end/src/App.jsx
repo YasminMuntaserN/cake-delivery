d10370e9fee9c400a39951e0e978b43534126f59
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-10-07 17:07:01 +0300

    feat: add api Categories

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,56 +1,27 @@
 import AppLayout from "./components/common/AppLayout";
-import { BrowserRouter, createBrowserRouter, Navigate, Route, RouterProvider, Routes } from "react-router-dom";
+import { BrowserRouter,  Navigate, Route,Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Cakes from "./pages/Cakes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const router =createBrowserRouter([
-  {
-    element : <AppLayout />,
-    // errorElement : <Error />,
-    children :[
-  
-  {
-    path:'/',
-    element: <Home/>
-  },
-
-//   {
-//     path:'/menu',
-//     element: <Menu/>,
-//     loader :menuLoader,
-//     errorElement : <Error />,
-//   },
-//   {
-//     path:'/cart',
-//     element: <Cart/>
-//   },
-//   {
-//     path:'/order/new',
-//     element: <CreateOrder/>,
-//     action : createOrderAction
-//   },
-//   {
-//     path:'/order/:orderId',
-//     element: <Order/>,
-//     loader:orderLoader
-//   }
- ]
-}] );
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route element={<AppLayout/>}>
-          <Route index element={
-                  <Navigate replace to="home"/>}/>
-          <Route path="home" element={<Home />} />
-          <Route path="cakes" element={<Cakes />} />
-          </Route>
-        {/* <Route path="login" element={<Login />} />
-        <Route path="*" element={<PageNotFound />} /> */}
-    </Routes>
-</BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+          <Routes>
+            <Route element={<AppLayout/>}>
+                <Route index element={
+                        <Navigate replace to="home"/>}/>
+                <Route path="home" element={<Home />} />
+                <Route path="cakes" element={<Cakes />} />
+                </Route>
+              {/* <Route path="login" element={<Login />} />
+              <Route path="*" element={<PageNotFound />} /> */}
+          </Routes>
+      </BrowserRouter>
+  </QueryClientProvider>
   );
 
 }
