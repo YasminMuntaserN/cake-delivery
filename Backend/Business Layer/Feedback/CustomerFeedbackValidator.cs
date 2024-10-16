9a4a8a8e99ec0c36d07378d5bdf8143be35d2127
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-10-16 14:27:42 +0300

    feat: add customer-feedback-validator

---
+++ Backend/Business Layer/Feedback/CustomerFeedbackValidator.cs
@@ -0,0 +1,37 @@
+﻿using Business_Layer.Feedback.Business_Layer.Feedback;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Validation;
+
+namespace Business_Layer.Feedback
+{
+    public class CustomerFeedbackValidator : BaseValidator<CustomerFeedback>
+    {
+        public override ValidationResult Validate(CustomerFeedback feedback)
+        {
+            if (!HasValidCustomerID(feedback.CustomerID))
+                AddError("CustomerID is invalid.");
+
+            if (!HasValidFeedback(feedback.Feedback))
+                AddError("Feedback cannot be empty.");
+
+            if (!HasValidFeedbackDate(feedback.FeedbackDate))
+                AddError("FeedbackDate cannot be in the future.");
+
+            return Result;
+        }
+
+        private bool HasValidCustomerID(int customerId) => customerId > 0;
+
+        private bool HasValidFeedback(string feedback)
+        {
+            return !string.IsNullOrWhiteSpace(feedback);
+        }
+
+        private bool HasValidFeedbackDate(DateTime feedbackDate) => feedbackDate <= DateTime.Now;
+    }
+}
+
