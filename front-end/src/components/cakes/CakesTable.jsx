8c58462f6f16c45f3c78f37140fd95603b0f325c
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-10-17 13:54:56 +0300

    feat: add use-cake hook and use it in cake table

--- front-end/src/components/cakes/CakesTable.jsx
+++ front-end/src/components/cakes/CakesTable.jsx
@@ -1,27 +1,12 @@
-import { useQuery } from "@tanstack/react-query";
 import CakeElement from "./CakeElement";
-import {getCakes, getCakesByCategory } from "../../services/apiCakes";
-import { useParams } from "react-router-dom";
+
 import Loader from "../common/Loader";
 import Error from "../common/Error";
+import { useCakes } from "./cakeHooks/useCakes";
 
 
 function CakesTable() {
-    const { categoryId } = useParams(); // Get categoryId from URL
-
-    const { data: cakes = [], error, isLoading } = useQuery(
-        Number(categoryId) === -1
-            ? {
-                queryKey: ["cakes"],
-                queryFn: getCakes,
-            }
-            : {
-                queryKey: ['cakes', categoryId], // Unique key including categoryId
-                queryFn: () => getCakesByCategory(categoryId), // Fetch function
-            }
-    );
-
-
+    const {cakes ,error , isLoading}=useCakes();
     if (isLoading) return <Loader />;
     if (error) return <Error />;
 
---
+++ front-end/src/components/cakes/cakeHooks/useCakes.js
@@ -0,0 +1,21 @@
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import {getCakes, getCakesByCategory } from "../../../services/apiCakes";
+
+export function useCakes() {
+  const { categoryId } = useParams(); 
+
+  const { data: cakes = [], error, isLoading } = useQuery(
+      Number(categoryId) === -1
+          ? {
+              queryKey: ["cakes"],
+              queryFn: getCakes,
+          }
+          : {
+              queryKey: ['cakes', categoryId], 
+              queryFn: () => getCakesByCategory(categoryId), 
+          }
+  );
+
+  return {cakes ,error , isLoading};
+}
