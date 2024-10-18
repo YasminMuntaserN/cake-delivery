ce2cf4345b5e536eefa222600094dd634bd85ff6
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-10-18 11:55:26 +0300

    refactor: edit the button component to meet different situations

--- front-end/src/ui/Button.jsx
+++ front-end/src/ui/Button.jsx
@@ -1,10 +1,12 @@
-function Button({children ,className ,onClick}) {
+function Button({children ,type ="normal"  ,onClick}) {
   return (
-    <button className={`mt-6 px-2 py-2  bg-peach text-pink rounded-md shadow hover:bg-basic transition duration-300 ` +{className}}
+    <button className={type ==="normal" ?styledNormalButton:styledCircleButton}
     onClick={onClick}>
     {children}
   </button>
   )
 }
+const styledNormalButton =`mt-6 px-2 py-2  bg-peach text-pink rounded-md shadow hover:bg-basic transition duration-300 ` ;
 
+const styledCircleButton ='bg-peach ml-3 mt-3  rounded-full hover:Scale(50%) text-center p-3 text-sm';
 export default Button
