af3544a786b133145fe67209ae854d4bdfe25a8e
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-10-20 14:29:32 +0300

    feat: add api gecoding to get the customer address

---
+++ front-end/src/services/apiGeocoding.js
@@ -0,0 +1,9 @@
+export async function getAddress({ latitude, longitude }) {
+  const res = await fetch(
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`
+  );
+  if (!res.ok) throw new Error("Failed to fetch address");
+
+  return await res.json();
+}
+
