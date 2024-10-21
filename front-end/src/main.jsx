309a522090d4e3dd0fabadcbb35641a006865f93
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-10-21 17:20:06 +0300

    refactor : edit the store by adding customerReducer

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -4,11 +4,29 @@ import { Provider } from "react-redux";
 import store from "./store"; 
 import App from './App.jsx'
 import './index.css';
+import { Toaster } from 'react-hot-toast';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   <Provider store={store}>
+  <Toaster position="top-center" gutter={12}
+      containerStyle={{marginTop:"100px"}}
+      toastOptions={{
+        success:{
+          duration: 3000,
+        },
+        error:{
+          duration: 5000,
+        },
+        style:{
+          fontSize:'16px',
+          maxWidth:'500px',
+          padding:'16px 24px',
+          backgroundColor:"black",
+          color:"pink",
+        }
+      }}/>
     <App />
   </Provider>,
   </StrictMode>,
--- front-end/src/store.js
+++ front-end/src/store.js
@@ -1,10 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit";
 import cartReducer from "./components/cart/cartSlice";
+import customerReducer from "./components/customer/customerSlice";
+
 
 
 const store =configureStore({
   reducer:{
     cart:cartReducer,
+    customer: customerReducer,
   }
 })
 
