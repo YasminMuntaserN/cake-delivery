2ba404e212cf51f268c5f1ba4cc39cdaa94ec81d
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-10-23 20:43:27 +0300

    feat:add use-order-Item hook

---
+++ front-end/src/components/order/hooks/useOrderItems.js
@@ -0,0 +1,27 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { addNewOrderItem } from "../../../services/apiOrderItem";
+import toast from "react-hot-toast";
+
+function useOrderItem() {
+  console.log("we are here in useOrderItem");
+  const queryClient = useQueryClient();
+
+const { mutate: newOrderItem, isLoading: isAdding } = useMutation({
+    mutationFn: addNewOrderItem,
+    onSuccess: (data) => {
+    toast.success("Order item added successfully");
+    queryClient.invalidateQueries({ queryKey: ["orderItems"] });
+    console.log(` Order item added successfully for ${data}`);
+    },
+    onError: (err) => {
+    toast.error(err.message || "An error occurred");
+    console.log(`order items ${err}`);
+
+    },
+});
+
+return { isAdding, newOrderItem }; 
+}
+
+
+export default useOrderItem
