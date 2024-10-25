9063db99cf8f8104d3a9d9d4c32c0b92e9fc7f3d
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-10-25 14:51:41 +0300

    feat: add stats component

---
+++ front-end/src/components/adminPanel/Stats.jsx
@@ -0,0 +1,11 @@
+function Stats({operation ,color}) {
+const StyledStat=` w-full bg-${color} border border-gray-200 rounded-md p-5 m-10 flex items-center gap-5`;
+  return (
+    <div className={StyledStat}>
+        {operation.icon}
+        <h3 className={StyledHeader}>{operation.name}</h3>
+    </div>
+  )
+}
+const StyledHeader="self-end text-sm uppercase tracking-wide font-semibold text-gray-500";
+export default Stats
