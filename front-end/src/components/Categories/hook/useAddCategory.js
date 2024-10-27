9b701bd88f8090c03af311b820d1e870292128a1
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-10-27 08:44:52 +0300

    feat: add use-add-category hook

---
+++ front-end/src/components/Categories/hook/useAddCategory.js
@@ -0,0 +1,22 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { addNewCategory } from "../../../services/apiCategories";
+
+export function useAddCategory() {
+const queryClient = useQueryClient();
+
+const { mutate: addCategory, isLoading: isAdding } = useMutation({
+    mutationFn: addNewCategory,
+    onSuccess: (data) => {
+    const newCategoryId = data.id;  
+    toast.success("Category added successfully");
+    queryClient.invalidateQueries({ queryKey: ["categories"] });
+    return newCategoryId;  
+    },
+    onError: (err) => {
+    toast.error(err.message || "An error occurred");
+    },
+});
+
+return { isAdding, addCategory }; 
+}
