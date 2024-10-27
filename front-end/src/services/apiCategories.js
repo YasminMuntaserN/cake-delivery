8b440f39e14a74735b38de32b88fd647b977d3d3
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-10-27 17:07:03 +0300

    feat: add editCategory and deleteCategory into category api

--- front-end/src/services/apiCategories.js
+++ front-end/src/services/apiCategories.js
@@ -1,5 +1,9 @@
-import { getAll ,addEntity } from "./BaseApi";
+import { getAll ,addEntity ,DeleteEntity ,EditEntity} from "./BaseApi";
 
 export const getAllCategories = async () => await getAll("categories");
 
 export const addNewCategory = async (categoryInfo) => await addEntity("categories" ,categoryInfo);
+
+export const editCategory = async (categoryInfo) => await EditEntity("categories",categoryInfo,categoryInfo.categoryID);
+
+export const deleteCategory = async (categoryId) => await DeleteEntity("categories",categoryId);
