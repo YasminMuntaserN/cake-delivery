8750057496865feaac60daf42eba87d0e7fe05fc
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-10-27 17:03:18 +0300

    feat: add AddEditCategoryForm component

---
+++ front-end/src/components/Categories/CategoriesTable.jsx
@@ -0,0 +1,28 @@
+
+import TableHeader from "../../ui/TableHeader";
+import Table from "../../ui/Table";
+import { useCategories } from "./hook/useCategories";
+import CategoryRow from "./CategoryRow";
+import Loader from "../common/Loader";
+import Error from "../common/Error";
+
+function CategoriesTable() { 
+    const {data:categories , error , isLoading}= useCategories();;
+        if (isLoading) return <Loader />;
+        if (error ) return <Error />;
+    return (
+            <Table>
+                <TableHeader gridColumns="grid-cols-[1fr_1fr_1fr_1fr]">
+                    <div>Image</div>
+                    <div>Category Name</div>
+                    <div>Delete</div>
+                    <div>Edit</div>
+                </TableHeader>
+                {categories.map((category) => (
+                    <CategoryRow  category={category}  key={category.categoryID} />
+                ))}
+            </Table>
+);
+}
+
+export default CategoriesTable
