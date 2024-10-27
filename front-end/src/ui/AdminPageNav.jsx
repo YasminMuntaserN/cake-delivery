cbbf0a0ef0f38388f96a6a3b6ac8ce2cdf4bbfb8
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-10-27 17:06:19 +0300

    refactor : edit the admin page nav by make array with all operation

--- front-end/src/ui/AdminPageNav.jsx
+++ front-end/src/ui/AdminPageNav.jsx
@@ -8,46 +8,52 @@ import {
   HiOutlineUsers,
 } from "react-icons/hi2";
 
+const pages=[
+  {
+    to:"/admin",
+    icon:<HiOutlineHome />,
+    name:"Dashboard"
+  },
+  {
+    to:"/adminCakes",
+    icon:<HiOutlineCalendarDays />,
+    name:"Cakes"
+  },
+  {
+    to:"/adminCategories",
+    icon:<HiOutlineHomeModern />,
+    name:"Categories"
+  },
+  {
+    to:"/adminCustomers",
+    icon:<HiOutlineUsers />,
+    name:"Customers"
+  },
+  {
+    to:"/adminUsers",
+    icon:<HiOutlineCog6Tooth />,
+    name:"Users"
+  }
+
+];
 function AdminPageNav() {
   return (
     <nav className="bg-gray-300 h-full">
       <Logo />
       <ul className={StyledList}>
+{     pages.map((page)=>(
         <li>
-          <NavLink className={StyledNavList} to="/admin">
-            <HiOutlineHome />
-            <span>Dashboard</span>
+          <NavLink className={({ isActive }) =>`flex pl-10 items-center gap-3 text-gray-800 text-base font-[20px] py-3 px-6 transition-all duration-300  hover:text-pink hover:bg-peach ${isActive &&'text-pink bg-gray-50'}`}
+            to={page.to}>
+            {page.icon}
+            <span>{page.name}</span>
           </NavLink>
         </li>
-        <li>
-          <NavLink className={StyledNavList} to="/adminCakes">
-            <HiOutlineCalendarDays />
-            <span>Cakes</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className={StyledNavList} to="/adminCategories">
-            <HiOutlineHomeModern />
-            <span>Categories</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className={StyledNavList} to="/adminCustomers">
-            <HiOutlineCog6Tooth />
-            <span>Customers</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className={StyledNavList} to="/adminUsers">
-            <HiOutlineUsers />
-            <span>Users</span>
-          </NavLink>
-        </li>
+))}
       </ul>
     </nav>
   );
 }
 const StyledList ="flex  flex-col gap-10";
-const StyledNavList ="flex pl-10 items-center gap-3 text-gray-800 text-base font-[20px] py-3 px-6 transition-all duration-300 rounded-lg hover:text-pink text-lg font-[600] hover:bg-peach";
 
 export default AdminPageNav
