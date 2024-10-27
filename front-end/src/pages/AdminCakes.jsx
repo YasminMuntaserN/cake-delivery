2950c79ebf4bc2a1036e9edc0783f56f0843dfeb
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-10-27 15:13:30 +0300

    feat: add admin cake component

---
+++ front-end/src/pages/AdminCakes.jsx
@@ -0,0 +1,11 @@
+import StockQuantity from "../components/adminPanel/StockQuantity"
+
+function AdminCakes() {
+  return (
+    <div>
+      <StockQuantity typeToUseWith="cakeTable"/>
+    </div>
+  )
+}
+
+export default AdminCakes
