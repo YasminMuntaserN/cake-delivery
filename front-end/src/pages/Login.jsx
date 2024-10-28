f3cad6f2d8c9e7a40c22a12cf20e01334eed7306
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-10-28 07:22:45 +0300

    refactor: edit the login page by adding logic to it

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,24 +1,37 @@
 import { useForm } from "react-hook-form";
-import AdminPageNav from "../ui/AdminPageNav"
-import Button from "../ui/Button"
-import Form from "../ui/Form"
-import FormRow from "../ui/FormRow"
+import Button from "../ui/Button";
+import Form from "../ui/Form";
+import FormRow from "../ui/FormRow";
+import { useNavigate } from "react-router-dom";
+import Logo from "../ui/Logo";
+import {useExistUser} from "../components/user/useExistUser";
+import toast from "react-hot-toast";
 
 function Login() {
   const { register, handleSubmit,formState }=useForm();
+  const {existUser}=useExistUser();
   const { errors } = formState;
+  const navigate =useNavigate();
   function onError(errors) { 
     console.log(errors);
 }
 function onSubmit(data) { 
   console.log(data);
+  if(data){
+    existUser(data ,{
+      onSuccess: (data) => {
+        console.log(data);
+        if(data ) navigate("/admin")
+          else  toast.error("Incorrect Password or Email🥲 ")
+      }});
+  }
 }
+
   return (
     <>
-    <AdminPageNav />
-
     <main className="mt-20 flex justify-center">
       <Form onSubmit={handleSubmit(onSubmit,onError )}>
+        <div className="flex justify-center mb-10" ><Logo /></div>
         <FormRow label="Email" error={errors?.email?.message}>
         <input
           className={StyledInput}
@@ -47,5 +60,5 @@ function onSubmit(data) {
     </>
   )
 }
-const StyledInput = "w-1/2 bg-transparent p-1 w-[300px] border rounded-md  border-red-200 ml-10 outline-none focus:ring focus:ring-pink focus:ring-opacity-50";
+const StyledInput = "w-1/2 bg-transparent p-1 w-[300px] border rounded-md  border-red-200 ml-5 outline-none focus:ring focus:ring-pink focus:ring-opacity-50";
 export default Login
